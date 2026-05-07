cfc5c6ff25efd8f2d958dd0bb6ff21313675133e
Johan Jonshede <jjonshede@outlook.com>
2026-05-07 16:06:41 +0200

    Before UI

--- Models/GameChoice.cs
+++ Models/GameChoice.cs
@@ -2,7 +2,7 @@
 {
     public class GameChoice
     {
-            public string Text { get; set; } = string.Empty;      // Det användaren ser på knappen
+            public string ButtonText { get; set; } = string.Empty;      // Det användaren ser på knappen
             public string NextNodeId { get; set; } = string.Empty; // Vart man hamnar
             public string? SummaryText { get; set; }              // För ChoiceHistory
             public string? RequiredFlag { get; set; }             // Valfritt: Krav för att se valet
